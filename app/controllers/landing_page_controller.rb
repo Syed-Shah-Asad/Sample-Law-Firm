9dd9fcfa3db9b21200a75a2b4197efd5ce8596e7
Syed Shah Asad <syed.asad@ebryx.com>
2016-12-04 17:38:10 +0500

    Removed Extra Files

--- app/controllers/landing_page_controller.rb
+++
@@ -1,5 +0,0 @@
-class LandingPageController < ApplicationController
-  def hello
-  	    @lawyers = Lawyer.all.sort_by &:case_type
-  end
-end
--- test/controllers/landing_page_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LandingPageControllerTest < ActionController::TestCase
-  test "should get hello" do
-    get :hello
-    assert_response :success
-  end
-
-end
